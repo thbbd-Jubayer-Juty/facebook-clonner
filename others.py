e470c58200fe690936a66b0ef1f63b5f2abaf8c0
It's Jubayer ❤️ <136098421+thbbd-Jubayer-Juty@users.noreply.github.com>
2023-07-02 14:23:03 +0600

    Add files via upload

---
+++ others.py
@@ -0,0 +1,6 @@
+import time,os 
+
+os.system("clear")
+print("....")
+time.sleep(0.5)
+os.system("python3 main.py")
---
+++ update.py
@@ -0,0 +1,6 @@
+import os,time
+
+os.system("clear")
+print("....")
+time.sleep(0.5)
+os.system("python3 main.py")
